dbfa9e16af384f2fcb5cdef0ba17c348da397835
taras_mac <frost-44@yandex.ru>
2026-04-02 21:28:44 +0300

    favicon installed

--- 1_chapter/main.py
+++ 1_chapter/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI, Query, Body
+from fastapi.responses import FileResponse
 import uvicorn
+from starlette.staticfiles import StaticFiles
+
 from hotels import router as router_hotels
 
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router_hotels)
 
+@app.get("/favicon.ico", include_in_schema=False)
+def get_favicon():
+    return FileResponse("static/favicon.ico")
 
 
 @app.get('/', summary="просто стартовая", description="описание сложной бизнес логиги")
